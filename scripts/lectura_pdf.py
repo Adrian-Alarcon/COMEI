b0af925ab22194a4119bae2ff3cf47e9cb6b452a
Admin23Adrian <adrian23.alarcon@gmail.com>
2022-04-20 09:44:40 -0300

    20-4: Se guarda el desarrollo antes de continuar

--- scripts/lectura_pdf.py
+++ scripts/lectura_pdf.py
@@ -4,57 +4,37 @@ import time
 
 
 def leer_pdf(ruta_pdf):
+    """Esta funcion se encarga de leer el pdf y devolver una nueva lista"""
     with open(ruta_pdf, "rb") as f:
         datos = slate3k.PDF(f)
-
     datos_2 = list(map(lambda x: x.replace("\n", " "), datos))
     datos_3 = datos_2[0].split("  ")
-
-    referencia_cod = datos_3.index("CÓDIGO")
-    referencia_inicio = referencia_cod + 6
-    referencia_fin = referencia_inicio + 4
-
-    ref_cod_af = datos_3.index("Afiliado:")
-    cod_af = datos_3[ref_cod_af + 1]
-
-    titulos = datos_3[referencia_cod:referencia_inicio]
-    descripciones = datos_3[referencia_inicio:referencia_fin]
-    autorizacion = datos_3[2]
-
-    print(datos_3)
-    # for dato in datos_3:
-    #     print(dato)
-    # print(descripciones)
-
-    # return descripciones, autorizacion, cod_af
+    return datos_3      #devuelvo una lista con toda la info del pdf
 
 
-def armar_descripciones(lista_desc):
-    cod_troq_y_desc = lista_desc.pop(0)
-    lista_troq_y_des = cod_troq_y_desc.split(" ")
-    troquel = lista_troq_y_des.pop(0)
-    descripcion = " ".join(lista_troq_y_des)
+# armar los datos de cada pdf
+def armar_datos(lista_filtrada):
+    datos = lista_filtrada[6:]
+    print(datos)
+    """
+    for i in range(len(datos)):
+        print(datos[i].split(" ", 1))
+    """
 
-    lista_desc.insert(0, troquel)
-    lista_desc.insert(1, descripcion)
 
-
+# --- PROGRAMA PRINCIPAL --- #
+MODELO_TITULOS = ['CÓDIGO', 'DESCRIPCION', 'CANTIDAD', '% COB.', 'ADM', 'MED']
 ruta_pdfs = "C:/Users/aalarcon/Desktop/OyP/IMPLEMENTACION CON GITHUB/COMEI/pdfs"
 
-for pdf in os.listdir(ruta_pdfs):
 
-    leer_pdf(ruta_pdfs + "/" + pdf)
-    print("")
-    # armar_descripciones(datos_pdf)
-    #
-    # troquel = datos_pdf[0]
-    # descripcion_material = datos_pdf[1]
-    # cantidad = int(float(datos_pdf[2]))
-    #
-    # print(f"Codigo de TROQUEL: {troquel}", end=" ")
-    # print(f"Descripcion de MATERIAL: {descripcion_material}", end=" ")
-    # print(f"CANTIDAD: {cantidad}", end=" ")
-    # print(f"NRO DE AUTORIZACION: {autorizacion}", sep=" -- ")
-    # print(f"Codigo de AFILIADO: {afiliado}", sep=" -- ")
-    # print("")
-    # print("")
+for pdf in os.listdir(ruta_pdfs):
+    # lista que viene con todos los datos del PDF
+    lista_datos = leer_pdf(f"{ruta_pdfs}/{pdf}")
+
+    # armamos una nueva lista filtrando los elementos a la derecha de la palabrea CÓDIGO
+    lista_filtrada = list(filter(lambda x: lista_datos.index(x) >= lista_datos.index(MODELO_TITULOS[0]), lista_datos))
+
+    # armamos otra lista para quedarnos solamente con los datos que nos interesan
+    lista_filtrada_2 = list(filter(lambda x: lista_filtrada.index(x) < lista_filtrada.index("AUTORIZADA AUTORIZADA"), lista_filtrada))
+    armar_datos(lista_filtrada_2)
+    print()
